f5601214f03255df1c4041aa340aba9ec4b8afba
Guilherme Felix <f3lixguilherme@gmail.com>
2026-01-19 19:02:48 -0300

    first commit - recpcao

---
+++ recepcao/src/main/java/com/estudos/ms/emergencia/recepcao/RecepcaoApplication.java
@@ -0,0 +1,13 @@
+package com.estudos.ms.emergencia.recepcao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecepcaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecepcaoApplication.class, args);
+	}
+
+}
---
+++ recepcao/src/test/java/com/estudos/ms/emergencia/recepcao/RecepcaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudos.ms.emergencia.recepcao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecepcaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
