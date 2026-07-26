144cdf556afae1f26d51a0c6b18e1f3dc4201326
Guilherme Felix <f3lixguilherme@gmail.com>
2026-07-26 17:30:21 -0300

    chore: adiciona módulo e dependências medicacao

---
+++ medicacao/src/main/java/com/estudos/ms/emergencia/medicacao/MedicacaoApplication.java
@@ -0,0 +1,13 @@
+package com.estudos.ms.emergencia.medicacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicacaoApplication.class, args);
+	}
+
+}
---
+++ medicacao/src/test/java/com/estudos/ms/emergencia/medicacao/MedicacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudos.ms.emergencia.medicacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
